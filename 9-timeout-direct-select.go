d365deeb0624e98652a099fa811af370799f6b23
kevchn <kevchen98@gmail.com>
2017-07-08 23:09:20 -0400

    add select concurrency patterns

---
+++ 9-timeout-direct-select.go
@@ -0,0 +1,34 @@
+//	Kevin Chen (2017)
+//	Patterns from Pike's Google I/O talk, "Go Concurrency Patterns"
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := generator("Hi!")
+	timeout := time.After(5 * time.Second)
+	for i := 0; i < 10; i++ {
+		select {
+		case s := <-ch:
+			fmt.Println(s)
+		case <-timeout: // time.After returns a channel that waits N time to send a message
+			fmt.Println("5s Timeout!")
+			return
+		}
+	}
+}
+
+func generator(msg string) <-chan string { // returns receive-only channel
+	ch := make(chan string)
+	go func() { // anonymous goroutine
+		for i := 0; ; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Second)
+		}
+	}()
+	return ch
+}
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Common and useful golang concurrency patterns I implemented from Rob Pike's famo
 [Google I/O Talk](https://www.youtube.com/watch?v=f6kdp27TYZs&t=1021s)
 
 ### Patterns
-Generator: function that runs goroutine and returns channel
-Multiplexing (fan-in): function that takes multiple channels and pipes to one channel, so that the returned channel receives both outputs
+- Generator: function that runs goroutine and returns channel
+- Multiplexing (fan-in): function that takes multiple channels and pipes to one channel, so that the returned channel receives both outputs
